3e0483fdcb5f6df420220e7e792dd79daa4c7802
Martin Kleiven <m.klei@live.no>
2023-03-07 10:01:38 +0100

    Implement basic JSON-stat adapter

---
+++ pages/api/json-stat.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import JSONstat from "jsonstat-toolkit";
+
+type Data = {
+  name: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+  let stat = await JSONstat(req.query.url)
+  let table = stat.Dataset(0).toTable({type: 'array'})
+  // Shift first column to last column
+  // to conform to everviz expectations
+  table.forEach((el) => {
+    let a = el.shift()
+    let b = el.pop()
+    el.push(a)
+    el.unshift(b)
+  });
+  res.send(table)
+}
